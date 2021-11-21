0e6b51eb4ab23f10ded346ed41cd1f3422979ff5
Daniil <Pandiara12@mail.ru>
2021-11-21 23:05:54 +0300

    first comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CollegeComponent } from './college/college.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { SpecializedComponent } from './specialized/specialized.component';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'college',
+    component: CollegeComponent,
+  },
+  {
+    path:'',
+    component:IntroductionComponent,
+  },
+  {
+    path: 'specialized',
+    component:SpecializedComponent,
+  },
+  {
+    path: 'test',
+    component:TestComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { CollegeComponent } from './college/college.component';
+import { SpecializedComponent } from './specialized/specialized.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent,
+    CollegeComponent,
+    SpecializedComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
